27a6a74bbc53aa08c834c3720fa60685f2e6c920
szymonides123 <szymek283@gmail.com>
2017-08-31 18:44:40 +0200

    comments

--- src/AppBundle/Controller/ShowPostController.php
+++ src/AppBundle/Controller/ShowPostController.php
@@ -15,31 +15,33 @@ use AppBundle\Entity\Comment;
 
 class ShowPostController extends Controller {
     
-    public function __construct(Posts $posts, Comment $comments ) {
-        $this->posts=$posts;
+    public function __construct(Comment $comments ) {
         $this->comment=$comments;  
       
     }   
     public function showAction($id, Request $request){
+//       znajdź post
         $post = $this->getDoctrine()
         ->getRepository('AppBundle:Posts')->find($id);
         if (!$post) {
         throw $this->createNotFoundException(
             'No product found for id '.$id
         );}
+//        znajdź komentarze do posta
         $em=$this->getDoctrine()->getManager();
         $commentquery = $em->createQuery(
                 'SELECT p From AppBundle:Comment p WHERE p.postId = :id'
                 )->setParameter('id', $id);
         $comment = $commentquery->getResult(); 
+//        stwórz formularz komentowania
         $form= $this->createForm(CommentForm::class, $this->comment);
+        
+//        obsługa wywołania formularza do komentowania
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $date = new \DateTime("now");
             $em = $this->getDoctrine()->getManager();
             $this->comment->setPostId($id);
-            $this->comment->setPublicationdate($date);
+            $this->comment->setPublicationdate(new \DateTime("now"));
             $this->comment->setAuthor($this->getUser());
             $em->persist($this->comment);
             $em->flush();
